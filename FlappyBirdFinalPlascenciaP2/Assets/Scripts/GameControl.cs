5f226f5624cc291e1d255f09830c641befd17c2f
Roman Plascencia <705392@student.dusd.net>
2022-12-20 10:43:11 -0800

    Finmmsihed game

--- FlappyBirdFinalPlascenciaP2/Assets/Scripts/GameControl.cs
+++ FlappyBirdFinalPlascenciaP2/Assets/Scripts/GameControl.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditorInternal;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TMP_Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -29,6 +36,16 @@ public class GameControl : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+
+    public void BirdScored()
+    {
+        if(gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score" + score.ToString();
     }
 
     public void BirdDied()
